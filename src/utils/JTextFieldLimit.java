ca446209fdec14e240534188d462457f4e9f7b79
iammadsnielsen <contact@madsnielsen.net>
2012-10-24 11:23:17 +0200

    Minor changes...

--- src/utils/JTextFieldLimit.java
+++ src/utils/JTextFieldLimit.java
@@ -13,6 +13,7 @@ import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.PlainDocument;
 
+@SuppressWarnings("serial") //removes annyong serial warning in eclipse....
 public class JTextFieldLimit extends PlainDocument
 {
     private int limit;
