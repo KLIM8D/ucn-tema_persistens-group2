6fe10e3ff0b3857ce044840d09d89d21e0ea4592
Chris <christind@hotmail.com>
2012-10-25 12:21:37 +0200

    Changed

--- src/db/DBProductCategory.java
+++ src/db/DBProductCategory.java
@@ -10,30 +10,35 @@ public class DBProductCategory implements IFDBProductCategory {
 	 {	
 		 _da = DataAccess.getInstance();
 	 }
+	 
 	@Override
 	public ArrayList<ProductCategory> getAllProductCategorys(
 			boolean retrieveAssociation) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
 	@Override
 	public ProductCategory getProductCategoryById(long id,
 			boolean retrieveAssociation) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
 	@Override
 	public ProductCategory getProductCategoryByName(String name,
 			boolean retrieveAssociation) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
 	@Override
 	public int insertProductCategory(ProductCategory productCategory)
 			throws Exception {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+	
 	@Override
 	public int updateProductCategory(ProductCategory productCategory) {
 		// TODO Auto-generated method stub
