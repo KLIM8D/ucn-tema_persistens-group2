82d39d01da8a429d97a6d7683c6565c08f7dac4c
iammadsnielsen <contact@madsnielsen.net>
2012-10-25 14:00:58 +0200

    Added customer interface, and db class....

---
+++ src/db/IFDBCustomer.java
@@ -0,0 +1,51 @@
+package db;
+
+import models.Customer;
+
+import java.util.ArrayList;
+
+public interface IFDBCustomer {
+	
+	/**
+	 * Retrieve all customers from database
+	 * 
+	 * @param retrieveAssociation 	set to true if customer data is wanted
+	 * @return ArrayList<Customer>
+	 */
+	public ArrayList<Customer> getAllCustomers(boolean retrieveAssociation);
+	
+	/**
+	 * Get specific customer by id
+	 * 
+	 * @param id					the id of the customer you need returned
+	 * @param retrieveAssociation	set to true if customer data is wanted
+	 * @return Customer
+	 */
+	public Customer getCustomerById(long id, boolean retrieveAssociation);
+	
+	/**
+	 * Get specific customer by contact name
+	 * 
+	 * @param name					the name of the customer you need returned
+	 * @param retrieveAssociation	set to true if customer data is wanted
+	 * @return Customer
+	 */
+	public Customer getCustomerByName(String name, boolean retrieveAssociation);
+	
+	/**
+	 * Inserts a new customer in the database
+	 * 
+	 * @param customer				the object containing the information you want stored
+	 * @return						returns the number of rows affected
+	 */
+	public int insertCustomer(Customer customer) throws Exception;
+	
+	/**
+	 * Update a existing contact in database
+	 * 
+	 * @param customer 				the object containing the updated information you want stored
+	 * @return						returns the number of rows affected
+	 */
+	public int updateCUstomer(Customer customer) throws Exception;
+
+}
