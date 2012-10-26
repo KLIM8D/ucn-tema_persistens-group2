4a7b18a1be74b25957788b8f48339f0b6003113d
Chris <christind@hotmail.com>
2012-10-26 09:43:26 +0200

    Created SupplierCtrl

---
+++ src/controllers/SupplierCtrl.java
@@ -0,0 +1,44 @@
+package controllers;
+
+import java.util.ArrayList;
+import models.Supplier;
+import db.DBSupplier;
+
+public class SupplierCtrl {
+
+	public SupplierCtrl()
+	{
+		
+	}
+	
+	public ArrayList<Supplier> getAllSuppliers(boolean retrieveAssociation) throws Exception
+	{
+		DBSupplier DBS = new DBSupplier();
+		ArrayList<Supplier> returnList = DBS.getAllSuppliers(retrieveAssociation);
+		return returnList;
+	}
+	
+	public Supplier getSupplierById(long id, boolean retrieveAssociation) throws Exception
+	{
+		DBSupplier DBS = new DBSupplier();
+		return DBS.getSupplierById(id, retrieveAssociation);
+	}
+	
+	public Supplier getSupplierByName(String name, boolean retrieveAssociation) throws Exception
+	{
+		DBSupplier DBS = new DBSupplier();
+		return DBS.getSupplierByName(name, retrieveAssociation);
+	}
+	
+	public int insertSupplier(Supplier supplier) throws Exception
+	{
+		DBSupplier DBS = new DBSupplier();
+		return DBS.insertSupplier(supplier);
+	}
+	
+	public int updateSupplier(Supplier supplier) throws Exception
+	{
+		DBSupplier DBS = new DBSupplier();
+		return DBS.updateSupplier(supplier);
+	}
+}
