6ee930595a52db6732b4775ccc3435191bdd4a92
iammadsnielsen <contact@madsnielsen.net>
2012-10-26 10:21:13 +0200

    Minor changes

--- src/models/Supplier.java
+++ src/models/Supplier.java
@@ -6,7 +6,7 @@ public class Supplier extends Contact
 	private String _contactPerson;
 	private String _bankAccount;
 	
-	public Supplier(String name, String address, int zipCode, String city, long phoneNo, String email, String country, String contactPerson, String bankAccount)
+	public Supplier(String name, String address, long zipCode, String city, long phoneNo, String email, String country, String contactPerson, String bankAccount)
 	{
 		super(name, address, zipCode, city, phoneNo, email, country);
 		_contactPerson = contactPerson;
